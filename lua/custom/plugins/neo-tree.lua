4edcc909225d6c53a32ec585144ea39c8803951b
mario corradi <mariocorradi@marios-MBP-2.station>
2024-05-23 11:16:55 +0200

    neo-tree catppucin

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -12,5 +12,9 @@ return {
       'MunifTanjim/nui.nvim',
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
+
+    config = function()
+      vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal left<CR>', {})
+    end,
   },
 }
