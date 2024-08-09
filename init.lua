74fdb8d1f210e8f8e6661e829b8710baa073a948
mario corradi <mariocorradi@marios-Laptop.station>
2024-08-09 14:20:31 +0200

    remove useless stuff

--- init.lua
+++ init.lua
@@ -572,7 +572,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         gopls = {},
-        golangci_lint_ls = {},
+        -- golangci_lint_ls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
